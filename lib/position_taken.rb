b13f68f7e974ee33e89eb2aee116b07b170ef962
Daniel Correa <dcorrea2207@gmail.com>
2020-02-02 17:04:05 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,17 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  ((board[index] == "X") || (board[ ("0"))
+ #  ((board[index] == "X") || (board[ ("0"))
+ # if board[index] == "X"
+ #  true 
+ #  elsif board[index] == "0"
+ #   true
+ # else
+ #   false 
+ # end
+ 
+  if board[index] == ""
+     false
 end
+ !(board[index] == " ")
+end
+ 
